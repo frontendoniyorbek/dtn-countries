87e5b549000263412eb39664c66d3a81a9267556
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-04-13 05:15:55 -0700

    Add site-header and img

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const body =  document.querySelector('body')
-const btnToggle = document.querySelector(".button")
+const btnToggle = document.querySelector(".site-header-theme-toggler")
 btnToggle.addEventListener('click'  , function(){
   body.classList.toggle('dark-mode')
-})
+});
