8e7d155b002439ba209b1b61d3d1272abd608c2b
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-05-09 21:36:10 -0700

    Add form bem

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const elSiteThemToggler =document.querySelector('.site-header-theme-toggler');
+const elSiteThemToggler =document.querySelector('.site-header__theme-toggler');
 elSiteThemToggler.addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
 });
